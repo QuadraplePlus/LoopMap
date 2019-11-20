62f261271d8af1a7659e01082014f10c111bc9f6
Jaeseong Lim <dlawotjd1020@gmail.com>
2019-11-20 16:18:29 +0900

    맵 스크롤 완료

---
+++ Assets/Scripts/LoopMaps.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoopMaps : MonoBehaviour
+{
+    [SerializeField] float speed = 1.5f;
+    [SerializeField] float startPos;
+    [SerializeField] float detPos;
+    // Start is called before the first frame update
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(-1 * speed * Time.deltaTime, 0, 0);
+
+        if (transform.position.x < detPos)
+        {
+            transform.Translate(-1 * (detPos - startPos), 0, 0);
+        }
+    }
+}
